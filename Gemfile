1195c54ad766a8698b10674109b1bf30da6652ec
Josh Leeman <joshleeman@gmail.com>
2015-02-06 20:28:55 -0500

    added bourbon/neat/bitters/refills, initial user model

--- Gemfile
+++ Gemfile
@@ -3,8 +3,13 @@ ruby '2.2.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
+gem 'bourbon'
+gem 'neat'
+gem 'bitters'
+gem 'refills'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+gem 'haml-rails'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150205053931) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.boolean  "admin"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
 end
