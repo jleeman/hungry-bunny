ec4e1c34f9136755174cca799ce3b1a01c92e4de
Josh Leeman <joshleeman@gmail.com>
2015-02-23 18:16:56 -0500

    fix event strong params

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:cocktail, :description, :title, :photo, :event_date, :event_time, :course1, :course2, :course3, :course4, :address, :dessert, :event_type)
+    params.require(:event).permit(:cocktail, :description, :title, :photo, :event_date, :event_time, :course_1, :course_2, :course_3, :course_4, :address, :dessert, :event_type)
   end
 end
