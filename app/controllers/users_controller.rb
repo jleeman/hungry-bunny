9c22f7301a9e8a13235dca0938adb2b9868bfe0b
Josh Leeman <joshleeman@gmail.com>
2015-02-23 15:50:31 -0500

    admin option to user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :admin)
   end
 end
