2888e102286c31930a2a73a91d572ed9f2566dcd
Josh Leeman <joshleeman@gmail.com>
2015-03-03 22:36:23 -0500

    refactor centered nav js to use ready function for both turbolinks events and GET requests

--- app/assets/javascripts/refills/centered_navigation.js
+++ app/assets/javascripts/refills/centered_navigation.js
@@ -1,4 +1,4 @@
-$(document).on('page:load', function() {
+var ready = function() {
   var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
   $('#js-centered-navigation-menu').removeClass("show");
 
@@ -10,18 +10,7 @@ $(document).on('page:load', function() {
       }
     });
   });
-});
+};
 
-$(document).ready(function() {
-  var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
-  $('#js-centered-navigation-menu').removeClass("show");
-
-  menuToggle.on('click', function(e) {
-    e.preventDefault();
-    $('#js-centered-navigation-menu').slideToggle(function(){
-      if($('#js-centered-navigation-menu').is(':hidden')) {
-        $('#js-centered-navigation-menu').removeAttr('style');
-      }
-    });
-  });
-});
+$(document).ready(ready);
+$(document).on('page:load', ready);
